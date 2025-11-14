c56c843bc9c4d72eae248ec86d2f470f1b2a42cb
Loic Leforestier <loicleforestier.pro@gmail.com>
2025-11-14 16:25:51 +0100

    init

---
+++ Assets/Scripts/EggBehavior.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EggBehavior : MonoBehaviour
+{
+    [SerializeField] private Rigidbody m_rigidBody;
+    [SerializeField] private float m_strength = 5;
+    [SerializeField] private float m_maxVelocity = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var force = Vector3.zero;
+        
+        //up
+        if (Math.Abs(m_rigidBody.linearVelocity.y) < m_maxVelocity)
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                force.y += m_strength;
+            }
+        }
+
+        //sides
+        if (Math.Abs(m_rigidBody.linearVelocity.x) < m_maxVelocity)
+        {
+            if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+            {
+                force.x += m_strength;
+            }
+            if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+            {
+                force.x -= m_strength;
+            }
+        }
+
+        //forward/backward
+        if (Math.Abs(m_rigidBody.linearVelocity.z) < m_maxVelocity)
+        {
+            if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+            {
+                force.z -= m_strength;
+            }
+            if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+            {
+                force.z += m_strength;
+            }
+        }
+
+        PushEgg(force);
+        print(m_rigidBody.linearVelocity);
+    }
+
+    void PushEgg(Vector3 direction)
+    {
+        m_rigidBody.AddForce(direction, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/cameraBehavior.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class cameraBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject m_gameObject;
+    [SerializeField] private GameObject player;
+    private Vector3 previousPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
