4d84154b80d03aed007da07498096b66d53243e2
Loic Leforestier <loicleforestier.pro@gmail.com>
2025-11-15 19:16:22 +0100

    added spline

--- Assets/Scripts/HandBehavior.cs
+++ Assets/Scripts/HandBehavior.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class HandBehavior : MonoBehaviour
 {
+    [SerializeField] private SplineContainer spline;
+    // [SerializeField] private float speed;
+    private float t = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,11 @@ public class HandBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime;
+        t %= 1f;
+
+        spline.Evaluate(t, out var position, out var tangent, out var up);
+        transform.position = position;
+        transform.rotation = Quaternion.LookRotation(tangent, up);
     }
 }
