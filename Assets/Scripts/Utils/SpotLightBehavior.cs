09dc02f23b8dc1c4c2d00c8d57eb6d26b17bc514
Loic Leforestier <loicleforestier.pro@gmail.com>
2025-12-08 18:11:03 +0100

    starting to work on light

---
+++ Assets/Scripts/Utils/SpotLightBehavior.cs
@@ -0,0 +1,14 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class SpotLightBehavior : MonoBehaviour
+{
+    [SerializeField] private Light spotLight;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spotLight.intensity = 0f;
+        spotLight.DOIntensity(100f, 2f).SetEase(Ease.OutExpo).SetDelay(.8f);
+    }
+}
